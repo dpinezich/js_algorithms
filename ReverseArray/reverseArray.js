a43a6e962b3c6866417121845d63429d70051150
David Pinezich <david.pinezich@gmail.com>
2019-06-06 21:04:41 +0200

    added reverse Array code

--- ReverseArray/reverseArray.js
+++ ReverseArray/reverseArray.js
@@ -1,5 +1,11 @@
 function reverseArrayInPlace(arr) {
-    for(let i = 0; i < arr.length; i++) {
-
+    for(let i = 0; i < arr.length / 2; i++) {
+        temporaryVar = arr[i];
+        arr[i] = arr[arr.length - 1 - i]; // important is the -i
+        arr[arr.length - 1 - i] = temporaryVar;
     }
+
+    return arr;
 }
+
+console.log(reverseArrayInPlace(['this', 'is', 'a', 'test']))
